ae2014d52e056633f32d43ac648665f84b4a6211
lovely <lovelyramghria39@gmail.com>
2026-03-13 13:37:05 +0530

    final change

--- app.py
+++ app.py
@@ -78,8 +78,11 @@ def home():
 
 
 # ✅ ALWAYS LAST
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
